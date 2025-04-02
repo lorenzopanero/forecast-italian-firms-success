defbc8febdc7d49dfee21ba3db4c07be4624236a
lorenzopanero <lorenzpaner@gmail.com>
2025-04-02 12:31:10 +0200

    Refactor data ingestion by updating file paths and replacing deprecated JSON file with a new structure

--- src/ingestion.py
+++ src/ingestion.py
@@ -4,7 +4,7 @@ import json # Open and read the JSON file
 
 # Funzione import csv
 def download_from_drive(csv_name):
-    with open("../data/file_id.json", "r", encoding="utf-8") as f: 
+    with open("data/csv_drive_id.json", "r", encoding="utf-8") as f: 
         data = json.load(f) # Print the data 
     file_id = data[csv_name]
     url = f"https://drive.google.com/uc?export=download&id={file_id}"
